1b4c08b2ddb5e499a396e8489f021cb4d2fd5abd
bikelaneblitz <bikelaneblitz@gmail.com>
2016-06-12 22:38:17 -0400

    compress before tweet, rxjs

--- index.js
+++ index.js
@@ -1,70 +1,69 @@
-var Twit = require('twit');
+const Twit = require('twit');
 const Rx = require('rx');
 const inquirer = require('inquirer');
 const config = require('./.settings.json');
 const fs = require('fs');
-
-var prompts = new Rx.Subject();
+const lwip = require('lwip');
 
 const templates = {
 	'cab_in_lane' : data => `cab in bike lane. complaint filed #bikenyc #visionzero #CyclistsWithCameras #nyc311 #${data.plate} #C1_1_${data.complaint}`
 };
 
-const promptPromise = inquirer.prompt(prompts);
-promptPromise.then(function(data){
-	console.log( templWithRemaining(data) );
-	var question = { name: 'tweet', type: 'confirm',  message: 'tweet?' };
-	inquirer.prompt([question]).then(function(answers){
-		console.log(answers);
-		if( answers.tweet ){
-			console.log('tweet it!');
-			tweetIt({photo:data.photo, msg:templ(data)});
-		}
-	});
+var questions = [
+	{ name: 'type', type: 'list', message: 'choose complaint', choices: Object.keys(templates) },
+	{ name: 'photo', message: 'photo path:', filter: str => str.trim() },
+	{ name: 'plate', message: 'plate #:'},
+	{ name: 'complaint', message: 'complaint #:'},
+	{ name: 'tweet', type: 'confirm', message: data => templWithRemaining(data)}
+];
+
+inquirer.prompt(questions).then(function(data){
+	console.log(data);
+	if( data.tweet ){
+		console.log('tweet it!');
+	tweetIt({photo:data.photo, msg:templates[data.type](data)});
+	}
 });
-prompts.onNext( { name: 'type', type: 'list', message: 'choose complaint', choices: Object.keys(templates) } )
-prompts.onNext( { name: 'photo', message: 'photo path:' });
-prompts.onNext( { name: 'plate', message: 'plate #:'});
-prompts.onNext( { name: 'complaint', message: 'complaint #:'} );
-
-prompts.onCompleted();
-
-const templ = function(data){
-	return `cab in bike lane. complaint filed #bikenyc #visionzero #CyclistsWithCameras #nyc311 #${data.plate} #C1_1_${data.complaint}`;
-};
 
 function templWithRemaining(data){
-	var msg = templ(data);
+	var msg = templates[data.type](data);
 	var remaining = `characters remaining:` + (140 - msg.length);
-	return msg + '\n' + remaining;
+	return msg + '\n' + remaining + '\ntweet?';
 };
-
-/*
-
-
-var T = new Twit( config );
-inquirer.prompt([{ name: 'plate', message: 'plate #:'},
-	{ name: 'complaint', message: 'complaint #:'}]).then(function(data){
-		console.log( `cab in bike lane. complaint filed #bikenyc #visionzero #CyclistsWithCameras #nyc311 #${data.plate} #C1_1_${data.complaint}` );
-	});
-*/
 
 function tweetIt(content){
 	console.log('tweetIt! start', content);
 	var T = new Twit( config );
-	var b64content = fs.readFileSync(content.photo.trim(), { encoding: 'base64' })
+	var lopen = Rx.Observable.fromNodeCallback( lwip.open );
 
-	T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-		var mediaIdStr = data.media_id_string;
-		var meta_params = { media_id: mediaIdStr }
-
-		T.post('media/metadata/create', meta_params, function (err, data, response) {
-			if (!err) {
-				var params = { status: content.msg, media_ids: [mediaIdStr] }
-				T.post('statuses/update', params, function (err, data, response) {
-					console.log(data)
-				});
-			}
+	lopen( content.photo ).flatMap( img => {
+		return Rx.Observable.create(function(obser){
+			img.toBuffer( 'jpg', {quality : 60 }, (err,buf) => {
+				obser.onNext(buf.toString('base64'))
+			});
 		});
-	});
+	}).flatMap( img_b64 => {
+		return Rx.Observable.create(function(obser){
+			T.post('media/upload', { media_data: img_b64 }, (err, data) => {
+				obser.onNext(data);
+			})
+		});
+	}).flatMap( data  => {
+		return Rx.Observable.create(function(obser){
+			T.post('media/metadata/create', { media_id : data.media_id_string }, function (err, createData, response) {
+				if( err ){
+					console.log( err );
+				} else{
+					obser.onNext(data);
+				}
+			});
+		});
+	}).flatMap( data  => {
+		return Rx.Observable.create(function(obser){
+			T.post('statuses/update', { status: content.msg, media_ids: [data.media_id_string] }, function (err, data, response) {
+				obser.onNext(data);
+			});
+		});
+	})
+	.subscribe( data => { console.log("DONEDONE"); console.log( data ) }, err => console.log("err"));
 };
