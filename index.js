131047fc6eac6aa258cfdba77bd71d2444fd28d1
bikelaneblitz <bikelaneblitz@gmail.com>
2016-06-29 22:51:45 -0400

    update starting to work

--- index.js
+++ index.js
@@ -38,20 +38,9 @@ inquirer.prompt(questions).then(function(data){
 	if( data.tweet ){
 		console.log('tweet it!');
 	tweetIt({photo:data.photo, msg:templates[data.complaint](data),comp_no:data.comp_no, flickr:data.flickr});
+	flickrUpload({photo:data.photo, msg:templates[data.complaint](data),comp_no:data.comp_no, flickr:data.flickr});
 	}
 });
-
-
-function createPrompt(){
-	console.log('inside createPrompt');
-	inquirer.prompt(questions).then(function(data){
-		console.log(data);
-		if( data.tweet ){
-			console.log('tweet it!');
-		tweetIt({photo:data.photo, msg:templates[data.complaint](data),complaint:data.complaint, flickr:data.flickr});
-		}
-	});
-};
 
 function templWithRemaining(data){
 	var msg = templates[data.complaint](data);
@@ -59,11 +48,7 @@ function templWithRemaining(data){
 	return msg + '\n' + remaining + '\ntweet?';
 };
 
-function tweetIt(content){
-	console.log('tweetIt! start', content);
-	var T = new Twit( config );
-	var lopen = Rx.Observable.fromNodeCallback( lwip.open );
-
+function flickrUpload(content){
 	var flickrOptions = {
 		api_key: config.flickr_key,
 		secret: config.flickr_secret,
@@ -90,7 +75,12 @@ function tweetIt(content){
 			console.log("photos uploaded");
 		});
 	});
+};
 
+function tweetIt(content){
+	console.log('tweetIt! start', content);
+	var T = new Twit( config );
+	var lopen = Rx.Observable.fromNodeCallback( lwip.open );
 
 	lopen( content.photo ).flatMap( img => {
 		return Rx.Observable.create(function(obser){
