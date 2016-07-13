ca84195361920d8159e92f09f418ebe1cd69a075
= <bikelaneblitz@gmail.com>
2016-07-13 13:04:00 -0400

    better error logging. misc fixes

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ const update_templates = {
 
 questions().then(function(data){
 	console.log(data);
-	if( data.type === 'Create' && data.tweet ){
+	if( data.type === 'create' && data.tweet ){
 		console.log('tweet it!');
 		tweet.tweetLocalImg( config, {photo:data.photo, msg:templates['create'][data.complaint](data),comp_no:data.comp_no, flickr:data.flickr})
-			.subscribe( data => { console.log("twitter post complete"); }, err => console.log("twitter err"));
-		flickr.upload(config, {photo:data.photo, msg:templates[data.complaint](data),comp_no:data.comp_no, flickr:data.flickr})
+			.subscribe( data => { console.log("twitter post complete"); }, err => { console.log("twitter err"); console.log( err ); });
+		flickr.upload(config, {photo:data.photo, msg:templates['create'][data.complaint](data),comp_no:data.comp_no, flickr:data.flickr})
 			.subscribe( data => { console.log("flickr upload complete"); }, err => console.log("flickr err"));
-	} else if ( data.type === 'Update' ){
+	} else if ( data.type === 'update' ){
 		console.log('update');
 		flickr.search( config )
 			.subscribe( data => { console.log("flickr search complete"); }, err => console.log("flickr err"));
